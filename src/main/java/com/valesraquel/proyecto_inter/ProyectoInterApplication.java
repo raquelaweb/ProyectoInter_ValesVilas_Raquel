db44de8c0b044c68ef565303626bcaf8b26ae8d8
Raquel <raquelaweb@gmail.com>
2026-04-25 13:54:41 +0200

    v1.7 - Comentario clase principal

--- src/main/java/com/valesraquel/proyecto_inter/ProyectoInterApplication.java
+++ src/main/java/com/valesraquel/proyecto_inter/ProyectoInterApplication.java
@@ -3,11 +3,12 @@ package com.valesraquel.proyecto_inter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Clase principal que arranca la aplicación Spring Boot
 @SpringBootApplication
 public class ProyectoInterApplication {
 
 	public static void main(String[] args) {
+		// Inicia la aplicación con todas sus configuraciones
 		SpringApplication.run(ProyectoInterApplication.class, args);
 	}
-
 }
